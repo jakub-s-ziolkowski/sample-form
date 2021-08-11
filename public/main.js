787b44da46da729eb48a8b766ccff316621b8477
Jakub Ziółkowski <jakub.s.ziolkowski@outlook.com>
2021-08-11 23:41:51 +0200

    Change inputs creation

--- public/main.js
+++ public/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-import { animatePlaceholderAll } from './scripts/placeholder-animation.js'
-import { animateForm } from './scripts/form-change-animation.js';
-import { validateForm } from './scripts/validation.js';
+import { createInputs } from './scripts/create-inputs.js';
 
-animatePlaceholderAll(document.querySelectorAll('.form__input'));
-animateForm(document.querySelector('.frame__trigger'));
-validateForm(document.querySelector('.form'));
+createInputs(document.querySelector('.form'), false);
--- public/scripts/create-inputs.js
+++ public/scripts/create-inputs.js
@@ -29,7 +29,43 @@ export const createInputs = (form, type) => {
 
         xhr.send();
     })
-    .then(content => console.log(content))
+    .then(data => {
+
+        const button = form.querySelector('.form__button');
+
+        data.forEach(element => {
+
+            if (type >= element.registrationOnly) {
+
+                const label = document.createElement('label');
+                label.appendChild(document.createTextNode(element.name));
+                label.classList.add('form__label');
+                label.setAttribute('for', element.name);
+
+                const input = document.createElement('input');
+                input.classList.add('form__input');
+                input.setAttribute('id', element.name);
+                input.setAttribute('name', element.name);
+                input.setAttribute('type', element.type);
+                input.setAttribute(
+                    'autocomplete',
+                    element.autoComplete.includes('/') ?
+                        element.autoComplete.split('-')[0].split('/')[type ? 1 : 0] + '-' + element.autoComplete.split('-')[1] :
+                        element.autoComplete);
+
+                const span = document.createElement('span');
+                span.classList.add('form__message');
+
+                const div = document.createElement('div');
+                div.classList.add('form__field');
+                div.appendChild(label);
+                div.appendChild(input);
+                div.appendChild(span);
+
+                form.insertBefore(div, button);
+            }
+        });
+    })
     .catch(error => console.error(error.statusText));
 
 };
