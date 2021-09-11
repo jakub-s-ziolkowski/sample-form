24b34b25dc699660fc0a7311e34f69e502b56517
Jakub Ziółkowski <jakub.s.ziolkowski@outlook.com>
2021-09-11 22:17:47 +0200

    File handler refactor

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 import http from 'http';
+import fs from 'fs';
 
 import * as signController from './app/signController.js';
-import { getFile } from './app/getFile.js';
 
 http.createServer((req, res) => {
 
@@ -24,29 +24,35 @@ http.createServer((req, res) => {
         const filePath = process.env.PWD + relativePath,
               extensionName = relativePath.split('.').pop();
 
-        getFile(
-            filePath,
-            content => {
+        new Promise((resolve, reject) =>
+            fs.readFile(filePath, (error, content) => {
 
-                let contentType;
+                if (!error && content) resolve(content);
+                else reject(error);
+            })
+        )
+        .then(content => {
 
-                switch (extensionName) {
+            let contentType;
 
-                    case 'html': contentType = 'text/html'; break;
-                    case 'css': contentType = 'text/css'; break;
-                    case 'js': contentType = 'text/javascript'; break;
-                    case 'json': contentType = 'application/json'; break;
-                    case 'svg': contentType = 'image/svg+xml'; break;
-                }
+            switch (extensionName) {
 
-                res.writeHead(200, {'Content-type': `${contentType}; charset = utf-8`});
-                res.end(content);
-            },
-            error => {
+                case 'html': contentType = 'text/html'; break;
+                case 'css': contentType = 'text/css'; break;
+                case 'js': contentType = 'text/javascript'; break;
+                case 'json': contentType = 'application/json'; break;
+                case 'svg': contentType = 'image/svg+xml'; break;
+            }
 
-                res.setHeader('Content-Type', 'text/plain');
-                res.end(`Error ${error}`);
-            });
+            res.writeHead(200, {'Content-type': `${contentType}; charset = utf-8`});
+            res.end(content);
+        })
+        .catch(error => {
+
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(`Error ${error}`);
+        });
+
     }
 
 }).listen(process.env.PORT);
